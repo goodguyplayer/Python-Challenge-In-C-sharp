2f9c09747406650528bab3bdfb8236cba4a8aa74
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-12-01 00:02:15 -0300

    Problem0 solved

--- PythonChallengeInCSharp/PythonChallengeInCSharp/Program.cs
+++ PythonChallengeInCSharp/PythonChallengeInCSharp/Program.cs
@@ -6,7 +6,25 @@ namespace PythonChallengeInCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            Problem0();
+            Console.ReadKey();
         }
+
+        // Okay, so here we go...
+        /* Problem 0, image of a 2^38
+         *
+         */
+        static void Problem0()
+        {
+            double Result, Number1, Number2;
+
+            Number1 = 2;
+            Number2 = 2;
+
+            Result = Math.Pow(Number1, Number2);
+            Console.WriteLine(Result);
+        }
+
     }
 }
