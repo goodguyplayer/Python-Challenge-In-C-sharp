0660136d091dd361a2b60abe67f0d9816b62b399
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-12-01 10:50:50 -0300

    Class RegexHandler Update - added method ReturnMatches
    Takes a pattern and returns all matches within a string

--- PythonChallengeInCSharp/PythonChallengeInCSharp/RegexHandler.cs
+++ PythonChallengeInCSharp/PythonChallengeInCSharp/RegexHandler.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace PythonChallengeInCSharp
 {
     class RegexHandler
     {
+        // For a collection of multiple matches within a string
+        public MatchCollection ReturnMatches(string pattern, string text)
+        {
+            Regex rgx = new Regex(pattern);
+
+            return rgx.Matches(text);
+        }
+
+
     }
 }
