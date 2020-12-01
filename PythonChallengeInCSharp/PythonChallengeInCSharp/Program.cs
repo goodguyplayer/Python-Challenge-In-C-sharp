ff5f40fece710795f9aca40f2b63e98268e012c7
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-11-30 23:54:32 -0300

    Project Start

---
+++ PythonChallengeInCSharp/PythonChallengeInCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PythonChallengeInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
